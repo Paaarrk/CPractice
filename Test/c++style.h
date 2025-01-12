a9554341f0cd4961b01011a87f62a6f4001f3e25
Paaarrk <lacogames87@gmail.com>
2025-01-12 20:47:48 +0900

    t status

--- Test/c++style.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CPLUSSTYLE_H__
-#define __CPLUSSTYLE_H__
-
-
-void CPlusStyle0();
-void CPlusStyle0Point();
-void BubbleSort();
-#endif
--- Test/cstyle.h
+++
@@ -1,7 +0,0 @@
-#ifndef __CSYTLE_H__
-#define __CSYTLE_H__
-
-//void CStyle0();
-void CStyle1();
-
-#endif
